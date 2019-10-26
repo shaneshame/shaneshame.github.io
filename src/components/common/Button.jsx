81494f5e13d2074a00ae3e6e9696b09193c93f98
Shane Schaefer <tf2shane@gmail.com>
2019-10-26 12:49:30 -0400

    Create Button and ButtonLink components

---
+++ src/components/common/Button.jsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  box-shadow: inset 0px 1px 0px 0px #9fb4f2;
+  background: linear-gradient(to bottom, #7892c2 5%, #476e9e 100%);
+  background-color: #7892c2;
+  border: 1px solid #4e6096;
+  display: inline-block;
+  cursor: pointer;
+  color: #ffffff;
+  font-weight: bold;
+  margin-left: 10px;
+  padding: 3px 10px;
+  text-decoration: none;
+  text-shadow: 0px 1px 0px #283966;
+
+  &:hover {
+    background: linear-gradient(to bottom, #476e9e 5%, #7892c2 100%);
+    background-color: #476e9e;
+  }
+
+  &:active {
+    position: relative;
+    top: 1px;
+  }
+`;
+
+export default Button;
