fe66398213d7ebc12cf83dec6ab0c81842f92f68
shaneshame <56442391+shaneshame@users.noreply.github.com>
2019-11-10 10:00:00 -0500

    Improve util functions and tests

--- src/utils/util.spec.js
+++ src/utils/util.spec.js
@@ -2,22 +2,36 @@ import { chunk, chunkIndices, chunkString, range, times } from './util';
 
 describe('range', () => {
   it('should create Array with only `stop`', () => {
-    const actual = range(3);
-    const expected = [0, 1, 2];
+    const actual = range(4);
+    const expected = [0, 1, 2, 3];
 
     expect(actual).toEqual(expected);
   });
 
   it('should create Array with `start` and `stop`', () => {
-    const actual = range(3, 6);
-    const expected = [3, 4, 5];
+    const actual = range(1, 5);
+    const expected = [1, 2, 3, 4];
 
     expect(actual).toEqual(expected);
   });
 
-  it('should create empty Array', () => {
+  it('should create Array with `start` and `stop` and `step` even', () => {
+    const actual = range(0, 20, 5);
+    const expected = [0, 5, 10, 15];
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('should create Array with `start` and `stop` and `step` uneven', () => {
+    const actual = range(0, 4, 2);
+    const expected = [0, 2];
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('should infer negative step when start > stop', () => {
     const actual = range(6, 3);
-    const expected = [];
+    const expected = [6, 5, 4];
 
     expect(actual).toEqual(expected);
   });
@@ -29,16 +43,30 @@ describe('range', () => {
     expect(actual).toEqual(expected);
   });
 
-  it('should create Array with `start` and `stop` and `step` even', () => {
-    const actual = range(0, 11, 2);
-    const expected = [0, 2, 4, 6, 8, 10];
+  it('should handle negative start', () => {
+    const actual = range(-4);
+    const expected = [0, -1, -2, -3];
 
     expect(actual).toEqual(expected);
   });
 
-  it('should create Array with `start` and `stop` and `step` uneven', () => {
-    const actual = range(0, 4, 2);
-    const expected = [0, 2];
+  it('should handle negative start with negative step', () => {
+    const actual = range(0, -4, -1);
+    const expected = [0, -1, -2, -3];
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('should handle a step of 0', () => {
+    const actual = range(1, 4, 0);
+    const expected = [1, 1, 1];
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('should handle a range of 0', () => {
+    const actual = range(0);
+    const expected = [];
 
     expect(actual).toEqual(expected);
   });
@@ -49,20 +77,6 @@ describe('times', () => {
     const actual = times(3, String);
     const expected = ['0', '1', '2'];
 
-    expect(actual).toEqual(expected);
-  });
-});
-
-describe('chunkIndices', () => {
-  it('should split evenly', () => {
-    const actual = chunkIndices(['a', 'b', 'c', 'd'], 2);
-    const expected = [[0, 1], [2, 3]];
-    expect(actual).toEqual(expected);
-  });
-
-  it('should split unevenly', () => {
-    const actual = chunkIndices(['a', 'b', 'c', 'd'], 3);
-    const expected = [[0, 1, 2], [3]];
     expect(actual).toEqual(expected);
   });
 });
@@ -121,6 +135,44 @@ describe('chunk', () => {
     const actual = chunk('123456789', 3);
     const expected = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']];
 
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe('chunkIndices', () => {
+  it('should split evenly', () => {
+    const actual = chunkIndices(['a', 'b', 'c', 'd'], 2);
+    const expected = [[0, 1], [2, 3]];
+    expect(actual).toEqual(expected);
+  });
+
+  it('should split unevenly', () => {
+    const actual = chunkIndices(['a', 'b', 'c', 'd'], 3);
+    const expected = [[0, 1, 2], [3]];
+    expect(actual).toEqual(expected);
+  });
+
+  it('should handle strings', () => {
+    const actual = chunkIndices('abcd', 2);
+    const expected = [[0, 1], [2, 3]];
+    expect(actual).toEqual(expected);
+  });
+
+  it('should handle invalid size', () => {
+    let actual = chunkIndices('abcd', 0);
+    const expected = [];
+    expect(actual).toEqual(expected);
+
+    actual = chunkIndices('abcd', -1);
+    expect(actual).toEqual(expected);
+
+    actual = chunkIndices('abcd', null);
+    expect(actual).toEqual(expected);
+  });
+
+  it('should handle no size', () => {
+    let actual = chunkIndices('abcd');
+    const expected = [[0], [1], [2], [3]];
     expect(actual).toEqual(expected);
   });
 });
