096787f71bd8a1e80e30ac4e3bf61e9f329c5958
shaneshame <56442391+shaneshame@users.noreply.github.com>
2020-01-30 11:05:56 -0500

    Create skeleton of server query page

--- src/components/TF2ServerQuery/TF2ServerQuery.jsx
+++ src/components/TF2ServerQuery/TF2ServerQuery.jsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
+import { Button, Header, Input, Label, Row } from '../common';
+
+const IP_EXAMPLE = '74.91.123.146:27015';
+
+const Form = styled.form``;
+
 const TF2ServerQuery = () => {
+  const [serverIP, setServerIP] = useState('');
+
+  const handleChangeServerIP = e => {
+    const newServerIP = e.target.value;
+    setServerIP(newServerIP);
+  };
+
+  const handleSubmit = () => {
+    console.log('Form submitted', 'IP:', serverIP);
+  };
+
   return (
     <div>
-      <h1>TF2 Server Query</h1>
+      <Header>TF2 Server Query</Header>
+      <Row>
+        <Form onSubmit={handleSubmit}>
+          <Label
+            css={`
+              display: inline-block;
+              margin-right: 10px;
+            `}
+            htmlFor="server-ip"
+          >
+            Server IP:
+          </Label>
+          <Input
+            id="server-ip"
+            onChange={handleChangeServerIP}
+            placeholder={IP_EXAMPLE}
+            type="text"
+            value={serverIP}
+          />
+          <Button role="submit">Search</Button>
+        </Form>
+      </Row>
     </div>
   );
 };
