55570fe0b17727c85a750e1dc8bcd138bc03f449
Shane Schaefer <tf2shane@gmail.com>
2020-04-14 15:15:50 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-66a06497e0f3d70c8659.js"
+    "url": "webpack-runtime-af1cbe763d1851ab585e.js"
   },
   {
     "url": "framework-84c9287a5714d2d8ce36.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c04c5f7d65138f24b92556e3625467e5"
+    "revision": "fb00eeb7b251f734758e132ac8a2c463"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-8acf230d498165638946.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "17fc138aa2cebdc1aefcbb3f04eaf5a2"
+    "revision": "d8b70268cdda086b18752a70f6f951a6"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
