e73bd6a1b77b832504c320d4bc492958905394d0
shaneshame <56442391+shaneshame@users.noreply.github.com>
2020-04-14 11:43:21 -0400

    Adding unfurl

--- src/pages/Spoonman.jsx
+++ src/pages/Spoonman.jsx
@@ -1,14 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet';
+import RoutineDesign from 'routine-design';
 import styled from 'styled-components';
+
+const routineDesign = new RoutineDesign();
 
 const serverIP = '74.91.115.39';
 const discordInviteLink = 'https://discord.gg/KH866DB';
 
+const windowSize = {
+  height: 400,
+  width: 400,
+};
+
+/* eslint-disable sort-keys */
 const colors = {
   brown: '#A52A2A',
   darkBrown: '##661515',
   darkBlue: '#7289DA',
   darkBlueHover: '#677BC4',
+  gray: '#8e9297',
   darkGray: '#2F3136',
   darkerGray: '#17181a',
   discordBlueLink: '#00B0F4',
@@ -18,6 +29,7 @@ const colors = {
   lightGray: '#36393F',
   white: '#FFFFFF',
 };
+/* eslint-enable sort-keys */
 
 const Container = styled.div`
   background-color: ${colors.lightGray};
@@ -42,11 +54,11 @@ const InnerBox = styled.div`
   box-shadow: 20px 20px 47px ${colors.darkerGray}, -20px -20px 47px #474a52;
   box-sizing: border-box;
   color: ${colors.green};
-  height: 400px;
+  height: ${windowSize.height}px;
   opacity: 0;
   padding: 1em;
   position: relative;
-  width: 400px;
+  width: ${windowSize.width}px;
   top: 45%;
   transition: opacity 0.2s ease;
   transform: translateY(-50%);
@@ -73,7 +85,16 @@ const ContentContainer = styled.div`
 const Title = styled.h1`
   margin: 0;
   padding: 0.5em 0 0.5em 0;
+  position: relative;
   text-transform: uppercase;
+
+  &::before {
+    bottom: 0;
+    content: '';
+    height: 1px;
+    position: absolute;
+    width: 50%;
+  }
 
   span {
     display: inline-block;
@@ -91,12 +112,13 @@ const Title = styled.h1`
 const codeStyles = `
   background-color: ${colors.darkerGray};
   border: none;
+  border-radius: 3px;
   color: inherit;
-  cursor: pointer;
+  cursor: input;
   font-family: monospace, monospace;
   font-size: 1rem;
   line-height: 1.625rem;
-  padding: 0.25rem;
+  padding: 0.35rem;
   text-align: center;
   width: 16rem;
 `;
@@ -110,7 +132,9 @@ const Code = styled.pre`
 `;
 
 const InfoLabel = styled.b`
-  color: ${colors.darkBlue};
+  color: ${colors.gray};
+  display: block;
+  margin-bottom: 2px;
 `;
 
 const Link = styled.a`
@@ -122,10 +146,38 @@ const Link = styled.a`
 
 const Label = styled.label`
   display: block;
-  letter-spacing: 1px;
+  font-family: Tahoma, Geneva, sans-serif;
+  font-size: 0.9rem;
   margin: 0.75rem 0;
   text-transform: uppercase;
 `;
+
+const MOTDUnfurlHeader = () => {
+  const [screenshot, setScreenshot] = useState();
+
+  useEffect(() => {
+    const puppeteer = require('puppeteer');
+    const browser = puppeteer.launch().then(() => {
+      const WebPage = routineDesign.createWebPage(
+        browser,
+        8080,
+        'path',
+        windowSize.width
+      );
+      setScreenshot(WebPage.screenshot());
+    });
+  }, []);
+
+  return (
+    <Helmet>
+      <meta content="https://shaneshame.github.io/Spoonman" property="og:url" />
+      <meta content="Spoonman TF2 Server" property="og:title" />
+      <meta content="Payload+ // Chicago" property="og:description" />
+      <meta content="Shane Shame" property="og:site_name" />
+      <meta content={screenshot} property="og:image" />
+    </Helmet>
+  );
+};
 
 const MOTD = () => {
   const [isSSR, setSSR] = useState(true);
@@ -136,6 +188,7 @@ const MOTD = () => {
 
   return (
     <Container>
+      <MOTDUnfurlHeader />
       <InnerBox className="isOpen">
         <ContentContainer>
           <Title>
